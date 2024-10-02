6b4070a0728ecce6b35cd996aeafac2e7b716e8f
Hazmat11 <etiennebertrand11@gmail.com>
2024-10-02 14:53:12 +0200

    First commit

---
+++ Assets/LookAt.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+
+    [SerializeField] private GameObject LookAtObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(LookAtObject.transform);
+    }
+}
