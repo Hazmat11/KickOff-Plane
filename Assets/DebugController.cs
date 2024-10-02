ed016017e695b30ab8d5fe9c98c501aaac4f4600
REYNOUD <o.reynoud@rubika-edu.com>
2024-10-02 17:02:03 +0200

    Debut de ld

---
+++ Assets/DebugController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugController : MonoBehaviour
+{
+    public Camera playerCam;
+
+    public float forwardSpeed;
+
+    public float sidewaysSpeed;
+    public float verticalSpeed;
+
+    public float mouseXSensitivity;
+    public float mouseYSensitivity;
+
+    public Rigidbody rb;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCam = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+
+    private Vector3 input;
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(0,
+            mouseXSensitivity * Input.GetAxis("Mouse X")
+            ,0);
+        
+
+        input = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            input = new Vector3(input.x, 0, input.z + forwardSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            input = new Vector3(input.x, 0, input.z - forwardSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            input = new Vector3(input.x - sidewaysSpeed, 0, input.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input = new Vector3(input.x + sidewaysSpeed, 0, input.z);
+        }
+
+
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            input = new Vector3(input.x, verticalSpeed, input.z);
+        }    
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            input = new Vector3(input.x, -verticalSpeed, input.z);
+        }
+
+        rb.velocity = input;
+
+    }
+}
